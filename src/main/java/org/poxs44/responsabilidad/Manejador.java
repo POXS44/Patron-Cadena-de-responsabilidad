70f9d85bb1717826cf845868423461557d7235ba
POXS44 <vzeledon7@gmail.com>
2021-11-04 20:51:36 -0600

    Clase manejador

---
+++ src/main/java/org/poxs44/responsabilidad/Manejador.java
@@ -0,0 +1,13 @@
+package org.poxs44.responsabilidad;
+
+public abstract class Manejador {
+    private Manejador sucesor;
+    public void setSucesor(Manejador sucesor) {
+        this.sucesor = sucesor;
+    }
+    public Manejador getSucesor() {
+        return sucesor;
+    }
+    public abstract double calculaCosteEnvio(Paquete p);
+}
+
