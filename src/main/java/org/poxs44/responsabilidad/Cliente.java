207b6a7d831f023d770346487f428898e31ab7e8
POXS44 <vzeledon7@gmail.com>
2021-11-04 22:21:07 -0600

    Algunos errores

--- src/main/java/org/poxs44/responsabilidad/Cliente.java
+++ src/main/java/org/poxs44/responsabilidad/Cliente.java
@@ -12,8 +12,8 @@ public class Cliente {
             manejador2.setSucesor(manejador3);
             //Calculamos cuanto nos cuesta enviar los paquetes
             Paquete p1 = new Paquete("Bicicleta", "Madrid Capital", 300);
-            Paquete p2 = new Paquete("Ordenador", "Comunidad Madrid", 20);
-            Paquete p3 = new Paquete("PDA", "Burgos", 1);
+            Paquete p2 = new Paquete("Ordenador", "Bonanza", 20);
+            Paquete p3 = new Paquete("PDA", "Rosita", 1);
             System.out.println("Coste envío paquete: \n" + p1.toString() + "\n" +
                     manejador1.calculaCosteEnvio(p1));
             System.out.println("Coste envío paquete: \n" + p2.toString() + "\n" +
